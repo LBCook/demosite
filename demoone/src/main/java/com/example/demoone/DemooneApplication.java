2a89e296306b405ae04cc028b8a0f7322774a25e
my_lin <linbin172053362@gmail.com>
2020-06-08 13:51:24 +0800

    简单的springboot项目，使用dockerfile打包

---
+++ demoone/src/main/java/com/example/demoone/DemooneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemooneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemooneApplication.class, args);
+    }
+
+}
---
+++ demoone/src/main/java/com/example/demoone/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demoone.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("test")
+    public String hello(){
+        return "hello world";
+    }
+}
